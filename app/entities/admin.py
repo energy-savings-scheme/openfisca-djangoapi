54b0c53be35455a796d964aa468a76948f01d1c7
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-03-01 17:14:13 +1100

    Cleanup // Edit `Entity` db model - make field names more meaningful - add help_text - add docstring

--- app/entities/admin.py
+++ app/entities/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from entities.models import Entity
 
+
 class EntityAdmin(admin.ModelAdmin):
-    list_display = ["key", "label", "is_person"]
+    list_display = ["name", "description", "is_person"]
+
 
 admin.site.register(Entity, EntityAdmin)
