62db193c8fc6e9d7ab29124b884ae0101762d38d
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-03-02 14:18:18 +1100

    Add search filter to Variables admin page

--- app/variables/admin.py
+++ app/variables/admin.py
@@ -6,6 +6,8 @@ from variables.models import Variable
 class VariableAdmin(admin.ModelAdmin):
     list_display = ["name", "description", "value_type", "definition_period"]
     filter_horizontal = ("dependencies",)
+    list_filter = ("entity",)
+    search_fields = ("name",)
 
 
 admin.site.register(Variable, VariableAdmin)
