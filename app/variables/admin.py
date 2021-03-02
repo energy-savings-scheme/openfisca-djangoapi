17ee7f80a982d3a31cc4be3a97c453e92b79acb7
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-03-02 12:20:51 +1100

    Style // apply Django admin widget for M2M field "dependencies" - improved readability

--- app/variables/admin.py
+++ app/variables/admin.py
@@ -5,6 +5,7 @@ from variables.models import Variable
 
 class VariableAdmin(admin.ModelAdmin):
     list_display = ["name", "description", "value_type", "definition_period"]
+    filter_horizontal = ("dependencies",)
 
 
 admin.site.register(Variable, VariableAdmin)
