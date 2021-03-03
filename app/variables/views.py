e6bb89c5aa3d273390773e51885b1c4140614f6f
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-03-03 11:50:44 +1100

    Docs // add docstrings for Variables endpoints

--- app/variables/views.py
+++ app/variables/views.py
@@ -10,12 +10,40 @@ from variables.serializers import VariableListSerializer, VariableChildrenSerial
 
 
 class VariablesList(generics.ListAPIView):
+    """
+    ## LIST all Variables stored in the database
+
+    ### Returns
+    - array of Variable objects (JSON)
+
+    ### Query params (optional)
+    This endpoint accept the following query params:
+    - search [str]: e.g "/variables?search=abc"
+    - is_final [bool]: e.g. "/variables?is_final=true"
+
+    Multiple queries can be combined with "&" (for example: "/variables?search=abc&is_final=true")
+
+    """
+
     queryset = Variable.objects.all()
     serializer_class = VariableListSerializer
     # pagination_class = LargeResultsSetPagination
 
 
 class VariableDetail(generics.RetrieveAPIView):
+    """
+    ## GET details of a single Variable
+
+    ### Returns
+    - a Variable object (JSON)
+    - or a 404 error if the specified Variable could not be found
+
+    ### URL parameter (required)
+    The following url parameter must be specified
+    - variable_name [str]: e.g. "/variables/<variable_name>/
+
+    """
+
     queryset = Variable.objects.all()
     serializer_class = VariableListSerializer
     lookup_field = "name"
@@ -23,6 +51,28 @@ class VariableDetail(generics.RetrieveAPIView):
 
 
 class VariableChildrenList(generics.RetrieveAPIView):
+    """
+    ## GET dependency tree of a single Variable
+
+    ### Returns
+    - a tree structure (in JSON format) recursively listing each child of the specified Variable, and each of <em>that variable's</em> children, etc.
+    - or a 404 error if the specified Variable could not be found
+    - the structrue of the tree is:
+        ```python
+        {  name: "abc",
+            children: [
+                        {name: "def" , children: [ ... ]},
+                        {name: "ghi" , children: [ ... ]},
+                      ]
+        }
+        ```
+
+    ### URL parameter (required)
+    The following url parameter must be specified
+    - variable_name [str]: e.g. "/variables/<variable_name>/children/
+
+    """
+
     queryset = Variable.objects.all()
     serializer_class = VariableChildrenSerializer
     lookup_field = "name"
