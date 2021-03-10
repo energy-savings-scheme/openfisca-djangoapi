5cc51e18e516ed77af08f0f4637d07f1b9cff7f8
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-03-11 09:36:57 +1100

    Rename migration

--- app/variables/migrations/0002_variable_formula.py
+++ app/variables/migrations/0002_variable_formula.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('variables', '0011_merge_20210310_0024'),
+        ("variables", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='variable',
-            name='formula',
+            model_name="variable",
+            name="formula",
             field=models.TextField(blank=True, null=True),
         ),
     ]
