f5272f1505565bb48a2b56df7a0edf09a816f49f
Yuguang Ipsen <Yuguang.Ipsen@gmail.com>
2021-03-10 21:54:38 +1100

    Update variables categories//differentiate D1 from D16

--- app/variables/management/commands/fetch_variables.py
+++ app/variables/management/commands/fetch_variables.py
@@ -195,6 +195,7 @@ class Command(BaseCommand):
             )
 
             # UpDating MetaData and parents relations
+            Variable.objects.all().update(metadata=None)
             metadata.updateByVariableTree()
             for entry in Variable.objects.all():
                 metadata.makeAlias(entry)
